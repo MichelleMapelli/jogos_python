0c166401dc59d4a57d3433a3d2bcd16582a1fcb1
Michelle <mi.mapelli@gmail.com>
2019-07-17 20:47:26 -0300

    segundo curso, forca

--- forca.py
+++ forca.py
@@ -5,6 +5,23 @@ def jogar_forca():
     print("***Novo jogo de FORCA***")
     print("************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto não False E não False // enquanto(True)
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
+
     print("Fim do Jogo!")
 
 if(__name__ == "__main__"):
